1cefb40eeac52f9e65c0c9096074a06856d71de2
GlazkovPavel <glazprom@mail.ru>
2021-06-19 22:36:23 +0300

    Refactoring

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,5 @@
-import { Card } from "./Card.js";
-import { FormValidator } from "./FormValidator.js";
+import { Card } from "../components/Card.js";
+import { FormValidator } from "../components/FormValidator.js";
 import Section from "../components/Section.js";
 import {
   openEditProfilePopupButton,
